ac0ad56af5b2f555447313d3467ba88673d6fd0e
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2023-09-28 18:02:16 +0100

    Task: Working on boiler plate

---
+++ src/Products/Product.jsx
@@ -0,0 +1,9 @@
+import './Products.css';
+
+const Product = () => {
+  return (
+    <div>Product</div>
+  )
+}
+
+export default Product
---
+++ src/Sidebar/Categories/Category.jsx
@@ -0,0 +1,9 @@
+import './Category.css'
+
+export const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category;
